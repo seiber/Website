6f8a019646a750dda5c25a32178ebb1ba3220b3e
Dakota Seiber <seiberdakota@gmail.com>
2021-02-07 22:46:33 -0600

    Home page
    -added in drop down menu
    -started 2nd page

--- js/test.js
+++ js/test.js
@@ -1,4 +1,18 @@
-document.getElementById("github").onclick = function()
+//calling when menu button is clicked
+function myFunction() 
 {
-    window.location.href = 'https://github.com/seiber';
+    //HTML ID = myDropdown, toggling "show" on CSS class "menu-content"
+    document.getElementById("myDropdown").classList.toggle("show");
 }
+window.onclick = function(event) {
+    if (!event.target.matches('.menu')) {
+      var dropdowns = document.getElementsByClassName("menu-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
