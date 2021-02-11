ecbee4e6103815d9896e48f41654c7fd5598a381
Dakota Seiber <seiberdakota@gmail.com>
2021-02-10 18:24:09 -0600

    Projects page
    -adding in projects page
    -skills
    -icon bar

--- js/test.js
+++ js/test.js
@@ -4,6 +4,19 @@ function myFunction()
     //HTML ID = myDropdown, toggling "show" on CSS class "menu-content"
     document.getElementById("myDropdown").classList.toggle("show");
 }
+var i = 0;
+var txt = 'My name is Dakota Seiber. I began studying Computer Science because I liked programming, and problem solving. This led me to achieve my Bachelors of Science studying Computer Science at University of Central Arkansas. Since graduating, I have worked at Conway Country Club, and began further improving my programming skillset. I post multiple times weekly on my github of the projects I am currently working on.';
+var speed = 40;
+
+window.onload = function typeWriter() 
+{
+  if (i < txt.length) 
+  {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
 window.onclick = function(event) {
     if (!event.target.matches('.menu')) {
       var dropdowns = document.getElementsByClassName("menu-content");
