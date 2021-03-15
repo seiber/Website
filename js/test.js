2d5075278ef780bdcd3ed9ff5e8fd8090b756a43
Dakota Seiber <seiberdakota@gmail.com>
2021-03-14 20:37:57 -0500

    Projects
    -added a new PHP contact-page replica project

--- js/test.js
+++ js/test.js
@@ -7,6 +7,8 @@ var modal3 = document.getElementById("mymodal3");
 var modal4 = document.getElementById("mymodal4");
 var modal5 = document.getElementById("mymodal5");
 var modal6 = document.getElementById("mymodal6");
+var modal7 = document.getElementById("mymodal7");
+
 
 
 // Get the button that opens the modal
@@ -17,6 +19,7 @@ var btn3 = document.getElementById("viewBTN3");
 var btn4 = document.getElementById("viewBTN4");
 var btn5 = document.getElementById("viewBTN5");
 var btn6 = document.getElementById("viewBTN6");
+var btn7 = document.getElementById("viewBTN7");
 
 
 var i = 0;
@@ -30,6 +33,7 @@ var span3 = document.getElementsByClassName("close")[3];
 var span4 = document.getElementsByClassName("close")[4];
 var span5 = document.getElementsByClassName("close")[5];
 var span6 = document.getElementsByClassName("close")[6];
+var span7 = document.getElementsByClassName("close")[7];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function()
@@ -65,6 +69,11 @@ btn6.onclick = function()
   modal6.style.display = "block";
 }
 
+btn7.onclick = function() 
+{
+  modal7.style.display = "block";
+}
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() 
@@ -95,6 +104,10 @@ span5.onclick = function()
 span6.onclick = function()
  {
   modal6.style.display = "none";
+}
+span7.onclick = function()
+ {
+  modal7.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -130,6 +143,11 @@ window.onclick = function(event) {
 window.onclick = function(event) {
   if (event.target == modal1) {
     modal6.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal7.style.display = "none";
   }
 }
 
